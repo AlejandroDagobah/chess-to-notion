e7842fc481a381d7bf6e800f3f29afb9c67a39f8
Dagobah <alejandrodagobah@gmail.com>
2022-10-28 15:45:57 -0500

    Agregar registro a la base de datos

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -7,6 +7,79 @@ const getBtn = document.getElementById('get')
 const postBtn = document.getElementById('post')
 
 
+async function userAction(url) {
+
+    const response = await fetch(url)
+    const jsonResponse = await response.json();
+
+    let chessGames = jsonResponse.games;
+
+    for (let i = 0; i < chessGames.length; i++) {
+        const currentGame = chessGames[i];
+        //console.log(currentGame)
+
+        const black = currentGame.black
+        const white = currentGame.white
+
+        const gameString = '🏆'
+        let winnerPlayer
+        let defeatedPlayer
+        let pgn = currentGame.pgn
+        let pgnArray = pgn.split('\n') 
+        let gameDate = pgnArray[2].substring(7, pgnArray[2].length - 2) //no sobreescribir en notion las que ya estan hechas
+        let gameTime = pgnArray[19].substring(10, pgnArray[19].length -2) //Formato para notion? //Condición si es mayor a 13:51:00
+        let termination = pgnArray[16].substring(14, pgnArray[16].length - 2) // eliminar la primera palabra de la razón de la victoria ej: 'won by checkmate'
+        let url = currentGame.url;
+
+        
+     
+        //#region Winner & Defeated conditions
+
+        if (white.result == 'win') {
+            winnerPlayer = white.username;
+            defeatedPlayer = black.username
+            
+        }else if (black.result == 'win') {
+            winnerPlayer = black.username;
+            defeatedPlayer = white.username
+
+
+        }else if(white.result == 'stalemate' || black.result == 'stalemate'){
+            winnerPlayer = 'ahogado';
+            defeatedPlayer = 'ahogado'
+
+        }else if(white.result == 'insufficient' || black.result == 'insufficient'){
+            winnerPlayer = 'draw';
+            defeatedPlayer = 'draw'
+
+        }else if(white.result == 'agreed' || black.result == 'agreed'){
+            winnerPlayer = 'agreed';
+            defeatedPlayer = 'agreed'
+
+        }
+
+        //#endregion
+
+        //#region Insert on table
+
+        const table = document.getElementById('tabla-registro');
+        
+        table.innerHTML += '<tr><td>'+ gameString +'</td><td>'+ winnerPlayer +'</td><td>'+ defeatedPlayer +'</td><td>'+ gameDate + ' | ' + gameTime +'</td><td>'+ termination +'</td><td><a href="' + url + '">' + url + '</a></td></tr>'
+
+        postInDB(gameString, winnerPlayer, defeatedPlayer, gameDate, termination, url)
+
+        //#endregion
+
+
+        console.log(winnerPlayer, defeatedPlayer, gameDate, gameTime, termination, url)
+        console.log(currentGame)
+    }
+    
+}
+
+
+
+/*
 getBtn.addEventListener('click', async function (e) {
     e.preventDefault()
 
@@ -20,10 +93,9 @@ getBtn.addEventListener('click', async function (e) {
 
     const data = await res.json()
     input.value = data.info
-    console.log(res)
-
     
 })
+*/
 
 postBtn.addEventListener('click', async function (e) {
     e.preventDefault()
@@ -31,21 +103,16 @@ postBtn.addEventListener('click', async function (e) {
     if(input.value == ''){return}
 
     const res = await fetch(baseUrl, {
-        
+
         method: 'POST',
         headers:{
             "Content-Type": 'application/json'
         },
         body: JSON.stringify({
-            parcel: input.value
+            value: input.value
         })
         
     })
-
-    console.log(res)
-
-    
-
     
 })
 
--- server.js
+++ server.js
@@ -38,290 +38,99 @@ app.get('/info', function (req, res) {
     
 })
 
-app.post('/', )
 
-app.post('/', jsonParser, (req, res) => {
-    const { parcel } = req.body
-
-    console.log(parcel)
-    if (!parcel) {
-
-        return res.status(400).send({status: 'failed'})
-        
-    }
-
-    res.status(200).send({status: 'recived'})
-
-})
-
-
-
-
-
-
-
-/*
 const notion = new Client({auth: "secret_Q9yioL3FNmSl7AsFL8JKwkeoUoUnoV8jsIJHfRxlZIM"});
 
 const databaseid = "7b1833b8cd2844fe880b6c2437910d3f";
-
-
 
 
 
 app.post('/', jsonParser, async function (req, res) {
 
-    const gameString = '🏆'
-    let winnerPlayer
-    let defeatedPlayer
-    let gameDate
-    let gameTime
-    let termination
-    let url
+/*
+    const gameString = req.body.gameString
+    let winnerPlayer = req.body.winnerPlayer
+    let defeatedPlayer = req.body.defeatedPlayer
+    let gameDate = req.body.gameDate
+    let gameTime = req.body.gameTime
+    let termination = req.body.termination
+    let url = req.body.url
+*/
+    const gameString = req.body
 
+    console.log(gameString)
+
+    if (!gameString) {
+
+        return res.status(400).send({status: 'failed'})
+        
+    }
+    res.status(200).send({status: 'recived'})
+
+    let winnerPlayer = req.body.winnerPlayer
+    let defeatedPlayer = req.body.defeatedPlayer
+    let gameDate = req.body.gameDate
+    let gameTime = req.body.gameTime
+    let termination = req.body.termination
+    let url = req.body.url
     try {
         
         const response = await notion.pages.create({
             parent: {database_id: databaseid},
             properties:{
-                Name:{
+                "Game":{
                     title:[
                         {
                             text:{
-                                content: gameString
+                                content: gameString.value
                             }
                         }
                     ]
                 },
                 "Winner":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: winnerPlayer
-                            }
-                        }
-                    ]
+                    select:{
+                        
+                        name: 'sami'
+
+                    }
+                    
                 },
                 "Defeated":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: defeatedPlayer
-                            }
-                        }
-                    ]
+                    select:{
+                        
+                        name: 'sami'
+
+                    }
+                    
                 },
                 "Date":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: gameDate + " | " + gameTime
-                            }
-                        }
-                    ]
+
+                    date:{
+                        
+                        start: "2022-10-29T11:00:00.000-04:00"
+
+                    }
+                    
                 },
                 "Termination":{
                     rich_text:[
                         {
                             text:{
-                                content: termination
+                                content: '🎃'
                             }
                         }
                     ]
                 },
                 "Link":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: url
-                            }
-                        }
-                    ]
-                }
-            }
-        })
-
-        console.log("SUCCES")
-
-    } catch (error) {
-        console.log(error)
-    }
-    
-})
-
-
-/*
-
-
-
-*/
-
-
-
-
-/*
-const {createReadStream} = require('fs');
-const http = require('http');
-const { Client } = require('@notionhq/client')
-
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  createReadStream('index.html').pipe(res)
-
-});
-
-const notion = new Client({auth: "secret_Q9yioL3FNmSl7AsFL8JKwkeoUoUnoV8jsIJHfRxlZIM"});
-
-const databaseid = "7b1833b8cd2844fe880b6c2437910d3f";
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  
-});
-
-
-
-
-async function userAction(url) {
-
-    const response = await fetch(url)
-    const jsonResponse = await response.json();
-
-    let chessGames = jsonResponse.games;
-
-    for (let i = 0; i < chessGames.length; i++) {
-        const currentGame = chessGames[i];
-        //console.log(currentGame)
-
-        const black = currentGame.black
-        const white = currentGame.white
-
-        const gameString = '🏆'
-        let winnerPlayer
-        let defeatedPlayer
-        let pgn = currentGame.pgn
-        let pgnArray = pgn.split('\n') 
-        let gameDate = pgnArray[2].substring(7, pgnArray[2].length - 2) //no sobreescribir en notion las que ya estan hechas
-        let gameTime = pgnArray[19].substring(10, pgnArray[19].length -2) //Formato para notion? //Condición si es mayor a 13:51:00
-        let termination = pgnArray[16].substring(14, pgnArray[16].length - 2) // eliminar la primera palabra de la razón de la victoria ej: 'won by checkmate'
-        let url = currentGame.url;
-
-     
-        //#region Winner & Defeated conditions
-
-        if (white.result == 'win') {
-            winnerPlayer = white.username;
-            defeatedPlayer = black.username
-            
-        }else if (black.result == 'win') {
-            winnerPlayer = black.username;
-            defeatedPlayer = white.username
-
-
-        }else if(white.result == 'stalemate' || black.result == 'stalemate'){
-            winnerPlayer = 'ahogado';
-            defeatedPlayer = 'ahogado'
-
-        }else if(white.result == 'insufficient' || black.result == 'insufficient'){
-            winnerPlayer = 'draw';
-            defeatedPlayer = 'draw'
-
-        }else if(white.result == 'agreed' || black.result == 'agreed'){
-            winnerPlayer = 'agreed';
-            defeatedPlayer = 'agreed'
-
-        }
-
-        //#endregion
-
-        //#region Insert on table
-
-        const table = document.getElementById('tabla-registro');
-        
-        table.innerHTML += '<tr><td>'+ gameString +'</td><td>'+ winnerPlayer +'</td><td>'+ defeatedPlayer +'</td><td>'+ gameDate + ' | ' + gameTime +'</td><td>'+ termination +'</td><td><a href="' + url + '">' + url + '</a></td></tr>'
-
-        postInDB(gameString, winnerPlayer, defeatedPlayer, gameDate, termination, url)
-
-        //#endregion
-
-
-        console.log(winnerPlayer, defeatedPlayer, gameDate, gameTime, termination, url)
-        console.log(currentGame)
-    }
-    
-}
-
-
-
-
-async function postInDB(gameString, winnerPlayer, defeatedPlayer, termination, url) {
-
-        const response = await notion.pages.create({
-            parent: {database_id: databaseid},
-            properties:{
-                Name:{
-                    title:[
-                        {
-                            text:{
-                                content: gameString
-                            }
-                        }
-                    ]
-                },
-                "Winner":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: winnerPlayer
-                            }
-                        }
-                    ]
-                },
-                "Defeated":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: defeatedPlayer
-                            }
-                        }
-                    ]
-                },
-                "Date":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: gameDate + " | " + gameTime
-                            }
-                        }
-                    ]
-                },
-                "Termination":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: termination
-                            }
-                        }
-                    ]
-                },
-                "Link":{
-                    rich_text:[
-                        {
-                            text:{
-                                content: url
-                            }
-                        }
-                    ]
+                    url:'https://www.chess.com/game/live/60616746903'
                 }
             }
         })
 
         console.log("SUCCESS")
 
+    } catch (error) {
+        console.log(error)
+    }
     
-}
-
-*/
+})
